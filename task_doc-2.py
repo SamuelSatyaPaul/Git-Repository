42462eb107ff82fd9d0c9e155633470e68a09098
SamuelSatyaPaul <satyapaul737@gmail.com>
2025-11-04 00:18:32 +0530

    Deleted 2 old Files

---
+++ task_doc-2.py
@@ -0,0 +1,55 @@
+# s = int(input("ent num:"))
+# if s % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# s = int(input("ent num:"))
+# if s % 5 == 0 and s%10 !=0:
+#     print("satisfied")
+# else:
+#     print("not satisfied")
+
+# a=int(input("sample of A :"))
+# b=int(input("sample of B :"))
+# if a > b :
+#     print("Greater")
+# else:
+#     print("Smaller")
+
+# a=int(input("sample of A :"))
+# b=int(input("sample of B :"))
+# if a < b :
+#     print("Greater")
+# else:
+#     print("Smaller")
+
+# s = int(input("ent num:"))
+# if s % 2 == 0 and s% 3 ==0 and s%6 ==0:
+#     print("satisfied")
+# else:
+#     print("not satisfied")
+
+# age = int(input("ent num:"))
+# if age >= 18:
+#     print(f"{age} satisfied for voting")
+# else:
+#     print( f"{age} not satisfied for voting")
+
+
+# maths_marks = int(input("enter marks:"))
+# phy_marks = int(input("enter marks:"))
+# chem_marks = int(input("enter marks:"))
+# if maths_marks >= 35 and phy_marks >= 35 and chem_marks >= 35:
+#     print("Passed")
+# else:
+#     print("Fail")
+
+# maths_marks = int(input("enter marks:"))
+# phy_marks = int(input("enter marks:"))
+# chem_marks = int(input("enter marks:"))
+# if maths_marks >= 35 or phy_marks >= 35 or chem_marks >= 35:
+#     print("Passed")
+# else:
+#     print("Fail")
+
