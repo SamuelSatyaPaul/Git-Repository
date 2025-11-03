1a6f7404cef2aff22cea531b6adf184ba60a402f
SamuelSatyaPaul <satyapaul737@gmail.com>
2025-11-03 23:38:27 +0530

    new file

---
+++ on22_09.py
@@ -0,0 +1,35 @@
+#Task -1
+s = 20
+sam = 7.67
+sum = s + sam
+print((type(sum)))
+
+#Task -2
+samuel = "2003"
+s_int = int(samuel)
+s_float = float(samuel)
+print(samuel,type(s_int))
+print(samuel,type(s_float))
+
+#Task -3
+x = 2
+y = 4
+x =x+y
+y =x-y
+x =x-y
+print(x,y)
+
+#Task -4
+v1 = 10
+v2 = 20
+v3 = 30
+result = (v1+v2+v3)/3
+print(result,type(result))
+
+#Task -5
+s1 = 28
+s2 = 20
+qt = s1 // s2
+rm = s1 % s2
+print(qt,type(qt))
+print(rm,type(rm))
